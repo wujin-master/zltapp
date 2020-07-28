f46667453d914a68f20dc0bfa6f4a7f57e896d41
munanfan <stoneman@17f.club>
2020-07-28 21:57:22 +0800

    add 聊天模块

---
+++ community/src/main/java/com/sqf/pojo/SendMessage.java
@@ -0,0 +1,30 @@
+package com.sqf.pojo;
+
+public class SendMessage {
+    private String head;
+    private String data;
+
+    public String getHead() {
+        return head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public SendMessage(String head, String data) {
+        this.head = head;
+        this.data = data;
+    }
+
+    public SendMessage() {
+    }
+}
