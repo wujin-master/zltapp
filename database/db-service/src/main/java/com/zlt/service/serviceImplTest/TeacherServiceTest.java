cde0713ccee2718caa5a88874aff585a6f865e53
Uryyyyyy <wujin1997@qq.com>
2020-07-29 21:35:48 +0800

    2020.07.29:部分测试类

---
+++ database/db-service/src/main/java/com/zlt/service/serviceImplTest/TeacherServiceTest.java
@@ -0,0 +1,35 @@
+package com.zlt.service.serviceImplTest;
+
+
+import com.zlt.pojo.EduTeacher;
+import com.zlt.service.TeacherService;
+import com.zlt.utils.UUIDUtil;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeacherServiceTest {
+    @Autowired(required = false)
+    private TeacherService EduTeacher;
+
+//    @Test
+//    public void addUser(){
+//        EduTeacher eduExam = new EduTeacher();
+//        eduExam.setExamId(UUIDUtil.getUUID());
+//        eduExam.setPaperId("111111");
+//        eduExam.setCourseId("教师测试用");
+//        int result = examService.addExam(eduExam);
+//        Assert.assertEquals(1,result);
+//    }
+
+    @Test
+    public  void findExamByid(){
+        EduTeacher eduExam = EduTeacher.findByTeacherId("01");
+        System.out.println(eduExam.getTeacherEmail());
+    }
+}
