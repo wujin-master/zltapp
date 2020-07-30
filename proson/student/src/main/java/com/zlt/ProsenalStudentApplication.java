e76b54f90028dc121588cf183875f9784f531705
Uryyyyyy <wujin1997@qq.com>
2020-07-30 12:00:53 +0800

    2020.07.30:2网关

--- proson/student/src/main/java/com/zlt/ProsenalStudentApplication.java
+++ proson/student/src/main/java/com/zlt/ProsenalStudentApplication.java
@@ -1,4 +1,15 @@
 package com.zlt;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker   //开启熔断
 public class ProsenalStudentApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ProsenalStudentApplication.class , args);
+    }
 }
---
+++ proson/student/src/main/java/com/zlt/controller/ProsenStudentComtroller.java
@@ -0,0 +1,35 @@
+package com.zlt.controller;
+
+import com.zlt.pojo.EduTeacher;
+import com.zlt.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("edu_teacher")
+public class ProsenStudentComtroller {
+
+    @Autowired(required=false)
+    private TeacherService teacherService;
+
+    @RequestMapping(value="/findAll",method={RequestMethod.GET})
+    @ResponseBody
+    public List<EduTeacher> queryUsers(){
+        return teacherService.findAllTeacher();
+    }
+
+    @RequestMapping(value="/upDate",method={RequestMethod.GET})
+    @ResponseBody
+    public void upDateTeacher(){
+        EduTeacher eduTeacher =  teacherService.findByTeacherId("01");
+        System.out.println(eduTeacher.getTeacherMobile());
+    }
+
+
+
+}
